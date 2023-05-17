c0055eec7e781f793d98c3d246f82c52e4960250
Harley Gotardo <106173250+HarleyGotardo@users.noreply.github.com>
2023-05-17 22:48:38 +0800

    Improved the overall UI and added the add expense feature

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
-//global vars
 let expenses = [];
 let totalAmount = 0;
 
-//ids from html
 const categorySelect = document.getElementById('category-select');
 const amountInput = document.getElementById('amount-input');
 const dateInput = document.getElementById('date-input');
@@ -14,8 +12,9 @@ const addIncomeBtn = document.getElementById('add-income-btn');
 const incomeInput = document.getElementById('income-input');
 const customCategoryInput = document.getElementById('custom-category-input');
 const addCategoryBtn = document.getElementById('add-category-btn');
+const deleteBtn = document.createElement('button');
 
-function addIncome() //add income function
+addIncomeBtn.addEventListener('click', function()
 {
     const incomeAmount = Number(incomeInput.value);
 
@@ -28,18 +27,36 @@ function addIncome() //add income function
     const currentBalance = parseFloat(totalBalance.textContent);
     const newBalance = currentBalance + incomeAmount;
     totalBalance.textContent = newBalance.toFixed(2);
-    incomeInput.value = ''; // Reset the income input field
-}
-
-addIncomeBtn.addEventListener('click', addIncome);
-
-
-addIncomeBtn.addEventListener('click', function() //implementation of addincome eventlistener
-{
-    const incomeAmount = Number(incomeInput.value);
-
-    totalAmount += incomeAmount;
-    totalAmountCell.textContent = totalAmount.toFixed(2);
-
-    incomeInput.value = ''; // Reset the income input field
+    incomeInput.value = ''; 
+});
+  
+addBtn.addEventListener('click', function () {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+  
+    if (category === '') {
+      alert('Please select a category');
+      return;
+    }
+    if (isNaN(amount) || amount <= 0) {
+      alert('Please enter a valid amount');
+      return;
+    }
+    if (date === '') {
+      alert('Please select a date');
+      return;
+    }
+  
+    const currentBalance = parseFloat(totalBalance.textContent);
+  
+    if (amount > currentBalance) {
+      alert('Expense amount is greater than the total balance');
+      return;
+    }
+  
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+  
+    totalBalance.textContent = (currentBalance - amount).toFixed(2);
 });
