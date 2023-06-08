79b94caa80122473d19df93b51b8777087154648
Harley Gotardo <106173250+HarleyGotardo@users.noreply.github.com>
2023-06-08 21:23:05 +0800

    Implemented a constraint in reflect feature. Reflect input value must be less than the current total balance value

--- src/script.js
+++ src/script.js
@@ -177,10 +177,11 @@ const reflectBtn = document.getElementById('reflect-btn');
 reflectBtn.addEventListener('click', function()
 {
 	const reflectAmount = Number(reflectInput.value);
+	const currentBalance2 = parseFloat(totalBalance.textContent);
 
-	if (isNaN(reflectAmount) || reflectAmount < 0 || reflectAmount == 0)
+	if (isNaN(reflectAmount) || reflectAmount < 0 || reflectAmount == 0 || reflectAmount > currentBalance2)
 	{
-		alert('Please enter a valid amount');
+		alert('Please enter a valid amount. Must also be less than the total balance.');
 		return;
 	}
 
